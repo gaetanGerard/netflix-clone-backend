3f1a94e7fd93f3064b546cb6f7133ca4a25bd8b8
gaetan <gaetan.gerard27@gmail.com>
2023-04-20 15:14:13 +0200

    Remove Force check on data to not be null as I cannot control the data

--- graphql/schema/tvEpisodes.js
+++ graphql/schema/tvEpisodes.js
@@ -1,18 +1,18 @@
 export const tvEpisodes = `
     """ Schema for One Episode of a Season """
     type TVEpisode {
-        air_date: String!
-        episode_number: Int!
-        crew: [Crew!]!
-        guest_stars: [GuestStar!]!
+        air_date: String
+        episode_number: Int
+        crew: [Crew!]
+        guest_stars: [GuestStar!]
         id: ID!
-        name: String!
-        overview: String!
-        production_code: String!
-        season_number: Int!
-        still_path: String!
-        vote_average: Float!
-        vote_count: Int!
+        name: String
+        overview: String
+        production_code: String
+        season_number: Int
+        still_path: String
+        vote_average: Float
+        vote_count: Int
         credits: TVEpisodeCredits
         images: TVEpisodeImages
     }
--- graphql/schema/tvSeasons.js
+++ graphql/schema/tvSeasons.js
@@ -2,13 +2,13 @@ export const tvSeasons = `
     """ Schema for a TV Season """
     type TVSeason {
         _id: ID!
-        air_date: String!
-        episodes: [TVEpisode!]!
-        name: String!
-        overview: String!
+        air_date: String
+        episodes: [TVEpisode]
+        name: String
+        overview: String
         id: ID!
         poster_path: String
-        season_number: Int!
+        season_number: Int
         aggregate_credits: AggregateCreditsTV
         credits: TVCredits
         images: TVImages
